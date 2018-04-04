477d03303f4b995aa85c67e70f9eb3513e3a5b43
daithi <webeire@gmail.com>
2018-04-04 20:20:08 +0100

    home component design

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Point of Sale app for cryptocurrencies
 
  - Vue2
  - MDL Lite
+
+### Running
+
+Development
+```bash
+npm run dev
+```
+
+Building
+```bash
+npm run build
+```
---
+++ src/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="container">
+
+    <div class="mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text" id="display">
+          Crypto POS
+        </h2>
+      </div>
+      <div class="mdl-card__supporting-text calc">
+        <div class="numbers">
+         <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="7">
+            7
+          </button>
+         <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="8">
+            8
+          </button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="9">
+            9
+          </button>
+          <br>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="4">
+            4
+          </button>
+         <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="5">
+            5
+          </button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="6">
+            6
+          </button>
+          <br>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="1">
+            1
+          </button>
+         <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="2">
+            2
+          </button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="3">
+            3
+          </button>
+          <br>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="0">
+            .
+          </button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="0">
+            0
+          </button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-func="number" data-val="0">
+            Go
+          </button>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
